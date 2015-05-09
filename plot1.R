6e9f9c49506718a8a943a602acf85f7986046b91
chtongueek <couchman_andrew@bah.com>
2015-05-09 13:37:45 -0400

    update plot files with .png commands

--- plot1.R
+++ plot1.R
@@ -4,10 +4,9 @@ datetime = strptime(paste(data[,1], data[,2]), "%Y/%m/%d %H:%m:%s")
 
 graphics.off()
 
-<<<<<<< HEAD
-hist(data[,3], xlab = "Global Active Power (kW)", col = '2')
-=======
+
 hist(data[,3], xlab = "Global Active Power (kW)", col = '2')
 
 dev.copy(png, "plot1.png")
->>>>>>> 1066656d644305b20f8b5b25720e71b8beb510d3
+
+dev.off()
--- plot2.R
+++ plot2.R
@@ -4,8 +4,7 @@ datetime = strptime(paste(data[,1], data[,2]), "%Y-%m-%d %H:%M:%S")
 
 graphics.off()
 
-<<<<<<< HEAD
 plot(datetime, data[,3], 'l', ylab = "Global Active Power, kW")
-=======
-plot(datetime, data[,3], 'l', ylab = "Global Active Power, kW")
->>>>>>> 1066656d644305b20f8b5b25720e71b8beb510d3
+dev.copy(png, "plot1.png")
+
+dev.off()
