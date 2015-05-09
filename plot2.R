1da1ee3cb7089b948a4b7973d0aaa2c7836841ff
chtongueek <couchman_andrew@bah.com>
2015-05-09 13:39:07 -0400

    more plot updates

--- plot2.R
+++ plot2.R
@@ -5,6 +5,6 @@ datetime = strptime(paste(data[,1], data[,2]), "%Y-%m-%d %H:%M:%S")
 graphics.off()
 
 plot(datetime, data[,3], 'l', ylab = "Global Active Power, kW")
-dev.copy(png, "plot1.png")
+dev.copy(png, "plot2.png")
 
 dev.off()
